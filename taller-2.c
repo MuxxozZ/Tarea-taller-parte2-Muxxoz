65cc94fb99e2cb5857d83ad566d3cdeb82740619
Rodrigo Muxxoz <rodrigomuxxoz01@gmail.com>
2024-08-25 01:55:51 -0400

    Se agrega suma y resta de 2 numeros naturales

---
+++ taller-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int a,b,suma,resta;
+	scanf("%d %d", &a,&b);
+	suma=a+b;
+	resta=a-b;
+	
+	printf("La suma de los numeros es: %d\n",suma);
+	printf("La resta de los numeros es: %d", resta);
+	
+	return 0;
+}
